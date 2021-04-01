fea7ababda53b6b6113d6f83fcbd507a2b407e48
ShahzadMujtaba <shahzadmujtaba9@gmail.com>
2021-04-01 13:34:16 +0530

    day second datastructure and algorith javascript

--- Palindroms/palindrome.js
+++ Palindroms/palindrome.js
@@ -1,12 +1,20 @@
 // example
 // palindrome("abba") === true
 // palindrome("abcdefg") === false
+
+// @desc solution 2
 function palindrome(str) {
-  // console.log(str);
-  const reversed_string = str
-    .split("")
-    .reduce((rev, curr_char) => curr_char + rev);
-  reversed_string === str ? console.log("true") : console.log("false");
+  const reversed_string = str.split("").every((char, index) => {
+    return char === str[str.length - index - 1];
+  });
+  console.log(reversed_string);
 }
-palindrome("abcdefg");
+
+// @desc Solution 1
+// function palindrome(str) {
+//   const reversed_string = str
+//     .split("")
+//     .reduce((rev, curr_char) => curr_char + rev);
+//   reversed_string === str ? console.log("true") : console.log("false");
+// }
 palindrome("abba");
